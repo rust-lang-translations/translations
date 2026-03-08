12a5a908ad635f9761bb1e232d1b4fdd5ea0cfc2
dalance <dalance@users.noreply.github.com>
2026-03-08 04:45:05 +0000

    deploy: de216cfb3081cc3388d9f04b0cee4383d02d3d19

--- rust-cli/ko/searcher-c2a407aa.js
+++ rust-cli/ko/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-5d9396b3.js',
+                path_to_root + 'searchindex-b06d8bdc.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
