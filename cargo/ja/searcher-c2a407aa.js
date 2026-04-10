72555a71a0324c5f5cadf1373a9ae5a26e27fc06
dalance <dalance@users.noreply.github.com>
2026-04-10 01:53:01 +0000

    deploy: cbce042a7ad5126e6444e30c2bd87db126f3795d

--- cargo/ja/searcher-c2a407aa.js
+++ cargo/ja/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-4532346c.js',
+                path_to_root + 'searchindex-6f9951ea.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
