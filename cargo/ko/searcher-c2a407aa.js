bbc4b1d677fcaa59a8a18335a268d0c3ed62233b
dalance <dalance@users.noreply.github.com>
2026-04-12 02:51:21 +0000

    deploy: de739310520c2eb7c8495e216aff029f07ee8328

--- cargo/ko/searcher-c2a407aa.js
+++ cargo/ko/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-3f7c0ac9.js',
+                path_to_root + 'searchindex-5c6b826c.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
--- reference/ko/searcher-c2a407aa.js
+++ reference/ko/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-849bf030.js',
+                path_to_root + 'searchindex-c818d889.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
