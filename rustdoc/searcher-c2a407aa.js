b155de3cdd653380daa93f161cfae88233c03f06
dalance <dalance@users.noreply.github.com>
2026-04-19 05:28:44 +0000

    deploy: 496b9aa9ea7372948a884cdff2c89d0d22f94591

--- rustdoc/searcher-c2a407aa.js
+++ rustdoc/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-8643dd18.js',
+                path_to_root + 'searchindex-9e78fedc.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
