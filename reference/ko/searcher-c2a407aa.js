66bc4d49e7a96fe659b81b03bd1b584eec50e69c
dalance <dalance@users.noreply.github.com>
2026-04-26 05:37:09 +0000

    deploy: f7759f4d55368697329d5ec9920b8e50c3b8be21

--- reference/ko/searcher-c2a407aa.js
+++ reference/ko/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-c818d889.js',
+                path_to_root + 'searchindex-e23b9ebb.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
--- reference/searcher-c2a407aa.js
+++ reference/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-203625ac.js',
+                path_to_root + 'searchindex-6050af5e.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
