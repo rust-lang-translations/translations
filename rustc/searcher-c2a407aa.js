12cff2fe71162e046f829b46aa6087a91c1dd176
dalance <dalance@users.noreply.github.com>
2026-05-31 06:59:57 +0000

    deploy: a71a1adb15158b1717d3b21765589c137fa4df0b

--- rustc/searcher-c2a407aa.js
+++ rustc/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-43f74028.js',
+                path_to_root + 'searchindex-e70dd3ff.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
