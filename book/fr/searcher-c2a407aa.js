f20567faf687769c05e096bc3842b4670db1a9cc
dalance <dalance@users.noreply.github.com>
2026-06-22 06:31:55 +0000

    deploy: 99926511006165f48cd909fcb17c7d6ff47a67ce

--- book/fr/searcher-c2a407aa.js
+++ book/fr/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-e705db65.js',
+                path_to_root + 'searchindex-aca41b98.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
