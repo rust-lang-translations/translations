5f00c34c43e25728aca8dad332b034f90734e93f
dalance <dalance@users.noreply.github.com>
2026-06-28 07:03:36 +0000

    deploy: 81ccd63c4106186bdfbaf46e2d9c755e711a8594

--- cargo/ja/searcher-c2a407aa.js
+++ cargo/ja/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-94bb5311.js',
+                path_to_root + 'searchindex-bd2f38ff.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
--- rustc/searcher-c2a407aa.js
+++ rustc/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-19498650.js',
+                path_to_root + 'searchindex-d22e0481.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
